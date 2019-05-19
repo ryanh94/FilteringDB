db7c58d9f94ddeb54a1d4e800a1277ce6e4c96ba
Ryan Hughes <RyanBH1994@hotmail.co.uk>
2019-05-19 20:59:27 +0100

    Program main refactor

--- Program.cs
+++ Program.cs
@@ -14,54 +14,37 @@ namespace Interview
             Console.WriteLine("Start of Application");
 
             var dataAccess = new DataAccess(ConfigHelper.ConnectionStrings.Demo);
-
-            var tableExists = dataAccess.TableExists<CommonColour>();
-
-            if (!tableExists)
-            {
-                // Migrate database to latest
-                var migrator = new Migrator(dataAccess);
-                migrator.CreateTable<CommonColour>();
-            }
-
             ColoursService coloursService = new ColoursService(dataAccess);
-            var commonColours = coloursService.GetCommonColours().ToList();
+
+            //Check if table exists if not then create
+            CreateNewTableIfDoesNotExist(dataAccess);
+
+            var commonColours = GetCommonColours(coloursService);
 
             Console.WriteLine($"{commonColours.Count} common colours found: ");
+
             foreach (var colour in commonColours)
             {
                 Console.WriteLine(colour);
             }
             Console.WriteLine();
-
             Console.Write($"Save these to the {nameof(CommonColour)}s table? (y/n) ");
 
+            //check for user input to save common colours
             var seedCommonColours = YesNo();
 
             if (seedCommonColours)
             {
+                //clear common colours in table
+                ClearDatabaseReadyForInsert(dataAccess);
+
                 Console.WriteLine("Clearing old data...");
-                dataAccess.Transaction<CommonColour>((ctx, table) =>
-                {
-                    table.DeleteAllOnSubmit(table);
-                });
-                Console.WriteLine("Common colours data cleared");
-
-                List<CommonColour> mappedCommonColours = new List<CommonColour>();
-
-                int index = 0;
-                foreach (var commonColour in commonColours)
-                {
-                    mappedCommonColours.Add(new CommonColour
-                    {
-                        ID = index++,
-                        Color = coloursService.ToRgb(commonColour)
-                    });
-                }
-
                 Console.WriteLine("Saving...");
-                dataAccess.InsertAll(mappedCommonColours);
-                Console.WriteLine($"Saved {mappedCommonColours.Count} common colours");
+
+                //insert new common colour colours
+                InsertCommonColours(dataAccess, coloursService, commonColours);
+
+                Console.WriteLine($"Saved {commonColours.Count} common colours");
             }
 
             ConsoleClock time = new ConsoleClock();
@@ -80,6 +63,60 @@ namespace Interview
 
             Console.WriteLine(keyPress);
             return keyPress == ConsoleKey.Y;
+        }
+        private static void CreateNewTableIfDoesNotExist(DataAccess dataAccess)
+        {
+            var tableExists = dataAccess.TableExists<CommonColour>();
+
+            if (!tableExists)
+            {
+                // Migrate database to latest
+                var migrator = new Migrator(dataAccess);
+                migrator.CreateTable<CommonColour>();
+            }
+        }
+        private static List<string> GetCommonColours(ColoursService coloursService)
+        {
+            var commonColours = coloursService.GetCommonColours().ToList();
+
+            return commonColours;
+        }
+        private static void ClearDatabaseReadyForInsert(DataAccess dataAccess)
+        {
+            dataAccess.Transaction<CommonColour>((ctx, table) =>
+            {
+                table.DeleteAllOnSubmit(table);
+            });
+        }
+        private static void InsertCommonColours(DataAccess dataAccess, ColoursService coloursServiceForRGB, List<string> commonColours)
+        {
+            List<CommonColour> mappedCommonColours = new List<CommonColour>();
+
+            int index = 0;
+            foreach (var commonColour in commonColours)
+            {
+                mappedCommonColours.Add(new CommonColour
+                {
+                    ID = index++,
+                    Color = coloursServiceForRGB.ToRgb(commonColour)
+                });
+            }
+            dataAccess.InsertAll(mappedCommonColours);
+        }
+        private static void InsertCommonColours(DataAccess dataAccess, List<string> commonColours)
+        {
+            List<CommonColour> mappedCommonColours = new List<CommonColour>();
+
+            int index = 0;
+            foreach (var commonColour in commonColours)
+            {
+                mappedCommonColours.Add(new CommonColour
+                {
+                    ID = index++,
+                    Color = commonColour
+                });
+            }
+            dataAccess.InsertAll(mappedCommonColours);
         }
     }
 }
